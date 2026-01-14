e3e98afc65caf5335b7eb0b9e65e2f3ce7126176
Mahmudul Islam Amit <164454497+mahmudul58@users.noreply.github.com>
2026-01-14 23:53:31 +0600

    Update DBUtil.java

--- BirthdayApp/src/main/java/com/example/birthdayapp/DBUtil.java
+++ BirthdayApp/src/main/java/com/example/birthdayapp/DBUtil.java
@@ -8,7 +8,7 @@ public class DBUtil {
 
     private static final String URL = "jdbc:mysql://localhost:3306/birthdaydb";
     private static final String USER = "root";
-    private static final String PASS = "Maiza@2023";
+    private static final String PASS = "DB_PASS";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
